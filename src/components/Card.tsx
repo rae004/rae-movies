7cf6b2bf3451358a5065fef14a321291ea4554b4
Robert Engel <robert.engel@apollidon.com>
2024-06-11 23:01:15 -0400

    Update card for movie on movies page.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -15,17 +15,24 @@ const Card = ({
   href: string;
 }) => {
   return (
-    <span className="p-5">
-      <Link href={href} className={'flex flex-col h-[450px] justify-between'}>
-        <h2 className=" text-xl line-clamp-2">{title}</h2>
+    <Link
+      href={href}
+      className="card bg-base-100 p-1 pb-4 group hover:bg-accent duration-500"
+    >
+      <div className="card-body px-4 pb-4">
+        <h2 className="text-xl line-clamp-2 group-hover:text-accent-content">
+          {title}
+        </h2>
+      </div>
+      <figure>
         <Image
           src={imgUrl + image}
           alt={`${title} Poster`}
           width={250}
           height={350}
         />
-      </Link>
-    </span>
+      </figure>
+    </Link>
   );
 };
 
