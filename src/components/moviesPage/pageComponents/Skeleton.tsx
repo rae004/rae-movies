dc83f466c8fe1090c051f9a3fdfd3c7e6b470b8a
Robert Engel <robert.engel@apollidon.com>
2024-07-05 18:26:09 -0400

    Fix movies page skeleton.

--- src/components/moviesPage/pageComponents/Skeleton.tsx
+++ src/components/moviesPage/pageComponents/Skeleton.tsx
@@ -17,7 +17,7 @@ const MoviesSkeleton = ({ noMovies }: { noMovies: boolean }) => {
           key={key}
           className="card bg-base-100 p-1 pb-4 group hover:bg-accent focus:bg-accent duration-500"
         >
-          <div className="card-body px-4 h-[105px]">
+          <div className="card-body pl-0 pr-4 h-[105px]">
             <div className="skeleton h-4 w-[248px]"></div>
             <div className="skeleton h-4 w-[196px]"></div>
           </div>
