fbf933942348e05e5432e631d666cb9f1275c58c
Robert Engel <robert.engel@apollidon.com>
2024-07-05 17:33:15 -0400

    Fix card height and skeleton on movies page.

--- src/components/moviesPage/pageComponents/Card.tsx
+++ src/components/moviesPage/pageComponents/Card.tsx
@@ -12,10 +12,13 @@ const Card = ({ title, image, href }: CardProps) => {
   return (
     <Link
       href={href}
-      className="card bg-base-100 p-1 pb-4 group hover:bg-accent focus:bg-accent duration-500 xl:w-[288px] xl:h-[508px]"
+      className="card bg-base-100 p-1 pb-4 group hover:bg-accent focus:bg-accent duration-500 xl:w-[288px] xl:h-[544px]"
     >
       <div className="card-body px-4 pb-4">
-        <h2 className="text-xl line-clamp-2 group-hover:text-accent-content">
+        <h2
+          className="text-xl line-clamp-2 group-hover:text-accent-content"
+          title={title}
+        >
           {title}
         </h2>
       </div>
