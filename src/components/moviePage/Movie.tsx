aeee6e0559e263bb48830db250dc6aebb0e555c0
Robert Engel <robert.engel@apollidon.com>
2024-07-09 18:28:49 -0400

    style fixes: add padding bottom and min height image for mobile. Genres flex wrap to show on mobile without scroll.

--- src/components/moviePage/Movie.tsx
+++ src/components/moviePage/Movie.tsx
@@ -26,7 +26,7 @@ export default function Movie({ data, isError, isLoading, slug }: MovieProps) {
   return (
     <div
       className={
-        'flex flex-col md:flex-row-reverse justify-between h-full m-1 md:py-10 gap-4'
+        'flex flex-col justify-between gap-4 h-full m-1 pb-20 md:flex-row-reverse md:py-10'
       }
     >
       <div>
--- src/components/moviesPage/pageComponents/MoviesGrid.tsx
+++ src/components/moviesPage/pageComponents/MoviesGrid.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 export default function MoviesGrid({ children }: { children: ReactNode }) {
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-2 gap-0 md:grid-cols-4 md:gap-4 md:p-10">
+      <div className="grid grid-cols-2 gap-0 pb-10 md:grid-cols-4 md:gap-4 md:p-10">
         {children}
       </div>
     </div>
